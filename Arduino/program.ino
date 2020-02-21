9b9dc1d5872410e057574310df3f72f9d72bae99
Diego Ulises <51557342+IDiegoUlises@users.noreply.github.com>
2020-02-21 20:36:20 -0300

    Update program.ino

--- Arduino/program.ino
+++ Arduino/program.ino
@@ -1,1 +1,23 @@
-**serial
+void setup() {
+  pinMode(2, INPUT_PULLUP);// pin 2 se conecta a gnd
+  pinMode(3, OUTPUT); // este pin se conecta al buzzer o el led
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  int estado = digitalRead(2);
+
+  if (estado == LOW) //HIGH
+  {
+    Serial.println("HIGH");
+    digitalWrite(3, LOW);
+  }
+
+  if (estado == HIGH) //LOW
+  {
+    Serial.println("LOW");
+    digitalWrite(3, HIGH);
+  }
+
+}
